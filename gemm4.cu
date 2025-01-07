76e13e65bba8d53e31048ab7862e58e4cec0e863
Rimika <rimikadhara67@gmail.com>
2025-01-07 00:42:42 -0600

    Minor fixes GeMM-4

--- gemm4.cu
+++ gemm4.cu
@@ -12,7 +12,7 @@
 #define tiles_Acols 8
 #define tiles_Bcols 64
 
-__global__ void coarse1D_mat_mul_kernel(float *d_A_ptr, float *d_B_ptr, float *d_C_ptr, 
+__global__ void coarse1D_mat_mul_kernel(float *d_A, float *d_B, float *d_C, 
                                     int C_n_rows, int C_n_cols, int A_n_cols)
 {
     const int b_x = blockIdx.x;
@@ -37,12 +37,12 @@ __global__ void coarse1D_mat_mul_kernel(float *d_A_ptr, float *d_B_ptr, float *d
     float value[COARSE_FACTOR] = {0.0f};
     for (int tile = 0; tile < num_tiles; tile++){
         if ((b_y * tiles_Arows + A_view_ty < C_n_rows) && ((tile * tiles_Acols + A_view_tx) < A_n_cols)){
-            sh_A[A_view_ty][A_view_tx] = d_A_ptr[(b_y*tiles_Arows + A_view_ty)*A_n_cols + (tile * tiles_Acols + A_view_tx)]; }
+            sh_A[A_view_ty][A_view_tx] = d_A[(b_y*tiles_Arows + A_view_ty)*A_n_cols + (tile * tiles_Acols + A_view_tx)]; }
         else
             { sh_A[A_view_ty][A_view_tx] = 0.0f; }
         
         if (((tile * tiles_Acols + B_view_ty) < A_n_cols) && (b_x * tiles_Bcols + B_view_tx < C_n_cols)){
-            sh_B[B_view_ty][B_view_tx] = d_B_ptr[(tile*tiles_Acols + B_view_ty) * C_n_cols + (b_x * tiles_Bcols + B_view_tx)];
+            sh_B[B_view_ty][B_view_tx] = d_B[(tile*tiles_Acols + B_view_ty) * C_n_cols + (b_x * tiles_Bcols + B_view_tx)];
         }
         else
             { sh_B[B_view_ty][B_view_tx] = 0.0f; }
@@ -61,16 +61,16 @@ __global__ void coarse1D_mat_mul_kernel(float *d_A_ptr, float *d_B_ptr, float *d
     // Storing and assigning
     for (int c = 0; c < COARSE_FACTOR; ++c){
         if ((row+c < C_n_rows) && (col < C_n_cols)){
-            d_C_ptr[(row+c)*C_n_cols + (col)] = 1*value[c] + 0*d_C_ptr[(row+c)*C_n_cols + (col)];
+            d_C[(row+c)*C_n_cols + (col)] = 1*value[c] + 0*d_C[(row+c)*C_n_cols + (col)];
         }
     } 
 }
 
 // function to invoke above CUDA kernel
-void gemm4(float *d_A_ptr, float *d_B_ptr, float *d_C_ptr, int C_n_rows, int C_n_cols, int A_n_cols){
+void gemm4(float *d_A, float *d_B, float *d_C, int C_n_rows, int C_n_cols, int A_n_cols){
     dim3 dim_grid(ceil(C_n_cols/(float)(tiles_Bcols)), ceil(C_n_rows/(float)(tiles_Arows)));
     dim3 dim_block(tiles_Arows * tiles_Bcols/COARSE_FACTOR);
-    coarse1D_mat_mul_kernel<<<dim_grid, dim_block>>>(d_A_ptr, d_B_ptr, d_C_ptr, C_n_rows, C_n_cols, A_n_cols);
+    coarse1D_mat_mul_kernel<<<dim_grid, dim_block>>>(d_A, d_B, d_C, C_n_rows, C_n_cols, A_n_cols);
 }
 
 int main() {
@@ -84,27 +84,27 @@ int main() {
     for (int index = 0; index < numSizes; index++) {
         int size = sizes[index];
 
-        float *d_A_ptr, *d_B_ptr, *d_C_ptr;
-        cudaMalloc((void **)&d_A_ptr, size * size * sizeof(float));
-        cudaMalloc((void **)&d_B_ptr, size * size * sizeof(float));
-        cudaMalloc((void **)&d_C_ptr, size * size * sizeof(float));
+        float *d_A, *d_B, *d_C;
+        cudaMalloc((void **)&d_A, size * size * sizeof(float));
+        cudaMalloc((void **)&d_B, size * size * sizeof(float));
+        cudaMalloc((void **)&d_C, size * size * sizeof(float));
 
-        float *h_A_ptr = new float[size * size];
-        float *h_B_ptr = new float[size * size];
+        float *h_A = new float[size * size];
+        float *h_B = new float[size * size];
         float *h_C_device = new float[size * size];
         float *h_C_host = new float[size * size];
 
         srand(42);
         for (int j = 0; j < size * size; j++) {
-            h_A_ptr[j] = static_cast<float>(rand()) / RAND_MAX;
-            h_B_ptr[j] = static_cast<float>(rand()) / RAND_MAX;
+            h_A[j] = static_cast<float>(rand()) / RAND_MAX;
+            h_B[j] = static_cast<float>(rand()) / RAND_MAX;
         }
 
-        cudaMemcpy(d_A_ptr, h_A_ptr, size * size * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_B_ptr, h_B_ptr, size * size * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_A, h_A, size * size * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_B, h_B, size * size * sizeof(float), cudaMemcpyHostToDevice);
 
         auto start = std::chrono::high_resolution_clock::now();
-        gemm4(d_A_ptr, d_B_ptr, d_C_ptr, size, size, size);
+        gemm4(d_A, d_B, d_C, size, size, size);
         cudaDeviceSynchronize();
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed = end - start;
@@ -113,13 +113,13 @@ int main() {
         double ops = 2.0 * size * size * size;
         gflops[index] = ops / (1e9 * times[index]);
 
-        cudaMemcpy(h_C_device, d_C_ptr, size * size * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_C_device, d_C, size * size * sizeof(float), cudaMemcpyDeviceToHost);
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 float sum = 0;
                 for (int k = 0; k < size; k++) {
-                    sum += h_A_ptr[i * size + k] * h_B_ptr[k * size + j];
+                    sum += h_A[i * size + k] * h_B[k * size + j];
                 }
                 h_C_host[i * size + j] = sum;
             }
@@ -141,12 +141,12 @@ int main() {
             std::cout << "\033[31mVerification Failed\033[0m\n";
         }
 
-        cudaFree(d_A_ptr);
-        cudaFree(d_B_ptr);
-        cudaFree(d_C_ptr);
+        cudaFree(d_A);
+        cudaFree(d_B);
+        cudaFree(d_C);
 
-        delete[] h_A_ptr;
-        delete[] h_B_ptr;
+        delete[] h_A;
+        delete[] h_B;
         delete[] h_C_device;
         delete[] h_C_host;
     }
